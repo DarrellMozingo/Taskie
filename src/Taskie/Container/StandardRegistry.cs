159939f3f66a4be65c0629eb27545316e35a4d8e
Darrell Mozingo <darrell@mozingo.net>
2010-12-16 11:58:34 -0500

    Made the registry public for StructureMap's scanning to pick up on.

--- src/Taskie/Container/StandardRegistry.cs
+++ src/Taskie/Container/StandardRegistry.cs
@@ -2,7 +2,7 @@ using StructureMap.Configuration.DSL;
 
 namespace Taskie.Container
 {
-	internal class StandardRegistry : Registry
+	public class StandardRegistry : Registry
 	{
 		public StandardRegistry()
 		{
