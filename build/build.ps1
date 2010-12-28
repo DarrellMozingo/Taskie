18f03cc3540e3cd54d623fda781a76ea906fa523
Darrell Mozingo <darrell@mozingo.net>
2010-12-28 11:15:30 -0500

    ILMerging StructureMap into the Taskie assembly, and creating a NuGet package.

--- build/build.ps1
+++ build/build.ps1
@@ -3,11 +3,13 @@ $ErrorActionPreference = "Stop"
 include ".\functions_fileIO.ps1"
 
 properties { # General
-	$solution_name = "Taskie"
+	$version = "0.1"
 	$script:configuration = "Debug"
 }
 
 properties { # Directories
+	$framework_dir = Get-FrameworkDirectory
+
 	$executing_directory = new-object System.IO.DirectoryInfo $pwd
 	$base_dir = $executing_directory.Parent.FullName
 
@@ -20,13 +22,17 @@ properties { # Directories
 
 	$build_artifacts_dir = "$base_dir\artifacts"
 	$build_output_dir = "$build_artifacts_dir\output"
-	$build_output_assemblies = "$build_output_dir\assemblies"
+	$build_output_build = "$build_artifacts_dir\output\build"
+	$build_output_assemblies = "$build_output_dir\required_assemblies"
+	$build_output_merged = "$build_output_dir\merged_assembly"
 	$build_output_nuget = "$build_output_dir\NuGet"
 	$build_reports_dir = "$build_artifacts_dir\reports"
 
 	$transient_folders = @($build_artifacts_dir, 
-						   $build_output_dir, 
+						   $build_output_dir,
+						   $build_output_build,
 						   $build_output_assemblies,
+						   $build_output_merged,
 						   $build_output_nuget,
 						   $build_reports_dir)
 }
@@ -34,20 +40,24 @@ properties { # Directories
 properties { # Executables
 	$tools_nunit = "$tools_dir\NUnit\nunit-console-x86.exe"
 	$tools_nuget = "$build_tools_dir\NuGet\NuGet.exe"
+	$tools_ilmerge = "$build_tools_dir\ILMerge\ILMerge.exe"
 }
 
 properties { # Files
-	$main_solution_file = "$source_dir\$solution_name.sln"
+	$main_solution_file = "$source_dir\Taskie.sln"
 	$sample_solution_file = "$sample_dir\StandardApp\src\StandardApp.sln"
 
-	$output_unitTests_dll = "$build_output_dir\$solution_name.UnitTests.dll"
+	$output_unitTests_dll = "$build_output_build\Taskie.UnitTests.dll"
 	$output_unitTests_xml = "$build_reports_dir\UnitTestResults.xml"
+
+	$merge_internalize_exclusions = "$build_dir\ilmerge_internalize_exclusions.txt"
+	$output_merged_assembly = "$build_output_merged\Taskie.dll"
 
 	$nuspec_template = "$build_dir\nuspec_template.nuspec"
 	$nuspec_file = "$build_output_nuget\taskie.nuspec"
 
-	$required_assemblies = @("$build_output_dir\$solution_name.dll",
-							 "$build_output_dir\StructureMap.dll")
+	$required_assemblies = @("$build_output_build\Taskie.dll",
+							 "$build_output_build\StructureMap.dll")
 }
 
 task set_to_release_mode {
@@ -65,26 +75,28 @@ task compile -depends clean {
 	$config = $script:configuration
 
 	echo "Compiling in $config mode."
-	exec { msbuild $main_solution_file /p:Configuration=$config /p:OutDir=""$build_output_dir\\"" /consoleloggerparameters:ErrorsOnly }
+	exec { msbuild $main_solution_file /p:Configuration=$config /p:OutDir=""$build_output_build\\"" /consoleloggerparameters:ErrorsOnly }
 	exec { msbuild $sample_solution_file /p:Configuration=$config /consoleloggerparameters:ErrorsOnly }
 }
 
 task unit_tests -depends compile {
-	exec { & $tools_nunit $output_unitTests_dll /nologo /xml=$output_unitTests_xml }
+	exec { & $tools_nunit $output_unitTests_dll /nologo /nodots /xml=$output_unitTests_xml }
 }
 
-task create_deployment -depends set_to_release_mode, unit_tests {
+task merge {
 	$required_assemblies | ForEach { copy_file $_ $build_output_assemblies }
 
-	create_nuget_package
+	exec { & $tools_ilmerge /targetplatform:"v4,$framework_dir" /log /out:"$output_merged_assembly" /internalize:$merge_internalize_exclusions $required_assemblies }
 }
 
-function create_nuget_package {
-	$required_assemblies | ForEach { copy_file $_ $build_output_nuget }
-	
+task create_deployment -depends set_to_release_mode, unit_tests, merge {
 	$nuspec = [xml](Get-Content $nuspec_template)
-	$nuspec.get_DocumentElement().metadata.version = "1.0"
+	$nuspec.package.metadata.version = $version
 	$nuspec.Save($nuspec_file)
 
-	exec { & $tools_nuget pack $nuspec_file -o $build_output_nuget }
+	exec { & $tools_nuget pack $nuspec_file -b $build_output_merged -o $build_output_nuget }
+}
+
+function Get-FrameworkDirectory {
+    $([System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory().Replace("v2.0.50727", "v4.0.30319"))
 }
--- build/functions_fileIO.ps1
+++ build/functions_fileIO.ps1
@@ -1,14 +1,11 @@
-function delete_directory($directory_name)
-{
+function delete_directory($directory_name) {
 	rm -Force -Recurse $directory_name -ErrorAction SilentlyContinue
 }
 
-function create_directory($directory_name)
-{
+function create_directory($directory_name) {
 	ni $directory_name -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 }
 
-function copy_file($source_file, $destination_directory)
-{
+function copy_file($source_file, $destination_directory) {
 	cp $source_file -Destination $destination_directory
 }
