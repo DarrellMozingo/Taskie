f8a2a9c1671bf1be59acf57da45462a5d4b610ac
Darrell Mozingo <darrell@mozingo.net>
2011-02-05 23:27:04 -0500

    Fixed build batch to work in a restricted permission environment.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 cls
-powershell -Command "& { Set-ExecutionPolicy Unrestricted; Import-Module .\build\tools\psake\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake '.\build\build.ps1' -framework 4.0 %*; Remove-Module psake }"
+powershell -ExecutionPolicy Unrestricted -NoProfile -Command "& { Import-Module '.\build\tools\psake\psake.psm1'; $psake.use_exit_on_error = $true; Invoke-psake '.\build\build.ps1' -framework 4.0 %*; Remove-Module psake }"
