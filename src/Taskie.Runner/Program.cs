b7dc6e98aebdb315c080d3423471de4f3c9170fa
Darrell Mozingo <darrell@mozingo.net>
2011-02-16 23:22:07 -0500

    Added runner executable.

---
+++ src/Taskie.Runner/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Taskie.Runner
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			// get ITaskieServiceLocator from external assembly (scan for it)
+			// call TaskieRunner as usual
+		}
+	}
+}
